9561246be2548a665856b194cf18ed326cd38e32
Samuel <67989204+SamuelFreitasSouza@users.noreply.github.com>
2023-09-07 16:04:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const painels = document.querySelectorAll('.painel');
+
+painels.forEach(painel =>{
+    painel.addEventListener('click',()=>{
+        removeActiveClass()
+        painel.classList.add('active')
+    })
+})
+
+function removeActiveClass(){
+    painels.forEach(painel =>{
+        painel.classList.remove('active')
+    })
+}
